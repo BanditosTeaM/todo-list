72a688608ab1d48394444c207396f32d9fcd794b
ltlaitoff <ltlaitoff@gmail.com>
2023-08-06 10:51:53 +0300

    feat(store): Created `store` initialize function

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import { getDatabaseData } from './api/getDatabaseOld'
 
+const STORAGE_TITLE_KEY = 'infoTitle'
+
 export const useDataStore = defineStore('data', {
 	state: () => ({
 		data: [],
@@ -12,10 +14,21 @@ export const useDataStore = defineStore('data', {
 		inputTask: ''
 	}),
 	actions: {
+		initialize() {
+			const storedTitles = localStorage.getItem(STORAGE_TITLE_KEY)
+			if (!storedTitles) return
+
+			const jsonTitles = JSON.parse(storedTitles)
+			if (!jsonTitles) return
+
+			this.title = jsonTitles
+		},
 		async fetchData() {
 			try {
 				this.data = await getDatabaseData()
-				this.title = this.data.titleTask
+				if (this.title.length === 0) {
+					this.title = this.data.titleTask
+				}
 				this.color = this.data.titleColor
 				this.task = this.data.infoTask
 			} catch (error) {
@@ -24,7 +37,7 @@ export const useDataStore = defineStore('data', {
 		},
 		deleteFolder(id) {
 			this.title = this.title.filter(title => title.id !== id)
-			localStorage.setItem('infoTitle', JSON.stringify(this.title))
+			localStorage.setItem(STORAGE_TITLE_KEY, JSON.stringify(this.title))
 		},
 		deleteTask(id) {
 			this.task = this.task.filter(task => task.id !== id)
@@ -47,8 +60,8 @@ export const useDataStore = defineStore('data', {
 		// 	this.addToLocalStorage()
 		// },
 		addToLocalStorage() {
-			localStorage.setItem('infoTitle', JSON.stringify(this.title))
-			const storedTitles = localStorage.getItem('infoTitle')
+			localStorage.setItem(STORAGE_TITLE_KEY, JSON.stringify(this.title))
+			const storedTitles = localStorage.getItem(STORAGE_TITLE_KEY)
 			this.title = JSON.parse(storedTitles)
 			console.log(this.title)
 		},
