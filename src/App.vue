ca57f03a08b6837e32dfcda2f9c44ff3115a6fe4
ltlaitoff <ltlaitoff@gmail.com>
2023-09-08 15:33:58 +0300

    style: Fixed button styles

--- src/App.vue
+++ src/App.vue
@@ -101,10 +101,16 @@ export default {
 							class="deleteTitle"
 							@click="onDeleteFolder(title.id)"
 						>
-							<img
-								src="./assets/image/hoverClose.svg"
-								alt="X"
-							/>
+							<svg
+								width="11"
+								height="11"
+								viewBox="0 0 11 11"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M6.87215 5.5L10.7129 1.65926C10.8952 1.47731 10.9977 1.23039 10.9979 0.972832C10.9982 0.715276 10.8961 0.468178 10.7141 0.285898C10.5321 0.103617 10.2852 0.00108525 10.0277 0.000857792C9.77011 0.000630336 9.52302 0.102726 9.34074 0.284685L5.5 4.12542L1.65926 0.284685C1.47698 0.102404 1.22976 0 0.971974 0C0.714191 0 0.466965 0.102404 0.284685 0.284685C0.102404 0.466965 0 0.714191 0 0.971974C0 1.22976 0.102404 1.47698 0.284685 1.65926L4.12542 5.5L0.284685 9.34074C0.102404 9.52302 0 9.77024 0 10.028C0 10.2858 0.102404 10.533 0.284685 10.7153C0.466965 10.8976 0.714191 11 0.971974 11C1.22976 11 1.47698 10.8976 1.65926 10.7153L5.5 6.87458L9.34074 10.7153C9.52302 10.8976 9.77024 11 10.028 11C10.2858 11 10.533 10.8976 10.7153 10.7153C10.8976 10.533 11 10.2858 11 10.028C11 9.77024 10.8976 9.52302 10.7153 9.34074L6.87215 5.5Z"
+								/>
+							</svg>
 						</button>
 					</li>
 				</ul>
@@ -141,15 +147,9 @@ export default {
 	height: 18px;
 	flex-shrink: 0;
 }
-.allTask:hover,
-.titleTask:hover {
+.allTask:hover {
 	border-radius: 4px;
 	background: #f8f6f4;
-	box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.05);
-}
-.deleteTitle:hover + .titleTask {
-	border-radius: 4px;
-	background: #cccccc;
 	box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.05);
 }
 
@@ -161,7 +161,10 @@ export default {
 	position: absolute;
 	margin-top: -40px;
 	margin-left: 190px;
+
+	fill: #e3e3e3;
 }
+
 .deleteTitle img {
 	width: 10px;
 	height: 10px;
@@ -193,6 +196,17 @@ export default {
 li:hover .deleteTitle {
 	opacity: 1;
 }
+
+.deleteTitle:hover {
+	fill: red;
+}
+
+li:hover .titleTask {
+	border-radius: 4px;
+	background: #f8f6f4;
+	box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.05);
+}
+
 .titleTask.router-link-active + .deleteTitle {
 	opacity: 1;
 }
