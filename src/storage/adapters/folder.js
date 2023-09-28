00c9cb5c1248e0d6e024799afec40fe079d01e39
BanditosTM <banditostm123@gmail.com>
2023-09-28 18:26:59 +0300

    rename variable

--- src/storage/adapters/folder.js
+++ src/storage/adapters/folder.js
@@ -1,13 +1,13 @@
 import storage from '../index'
 
-const STORAGE_folder_KEY = 'infoFolder'
+const STORAGE_FOLDER_KEY = 'infoFolder'
 
 function setFolderInStorage(value) {
-	storage.setItem(STORAGE_folder_KEY, JSON.stringify(value))
+	storage.setItem(STORAGE_FOLDER_KEY, JSON.stringify(value))
 }
 
 function getFolderInStorage() {
-	return storage.getItem(STORAGE_folder_KEY)
+	return storage.getItem(STORAGE_FOLDER_KEY)
 }
 
 export default {
